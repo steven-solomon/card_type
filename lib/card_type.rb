f82b202e0a868cc413fa95ef6ebc83d3648bde69
steven-solomon <solomon.steven.m@gmail.com>
2016-04-17 12:01:03 -0400

    Remove dead code paths

--- lib/card_type.rb
+++ lib/card_type.rb
@@ -24,33 +24,4 @@ class CardType
         UnknownCard.new
     end
   end
-
-  def initialize(card_number, security_code = nil, current_date = nil)
-    @security_code = security_code
-    @card_number = card_number
-    case
-      when Amex.is_card?(card_number)
-        @card = Amex.new(card_number)
-      when Discover.is_card?(card_number)
-        @card = Discover.new(card_number)
-      when Mastercard.is_card?(card_number)
-        @card = Mastercard.new(card_number, security_code, current_date)
-      when Visa.is_card?(card_number)
-        @card = Visa.new(card_number)
-      else
-        @card = UnknownCard.new
-    end
-  end
-
-  def charge(amount)
-    @card.charge(amount)
-  end
-
-  def name
-    @card.name
-  end
-
-  def return(receipt)
-    @card.return(receipt)
-  end
 end
